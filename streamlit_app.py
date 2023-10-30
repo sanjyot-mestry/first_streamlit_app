e95e37ee0abdbeff914f9c116ef6ebbdc1695c63
sanjyot-mestry <sanjyot.mestry@gmail.com>
2023-10-30 12:08:31 +0000

    Update streamlit_app.py
    Added diner menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title("Something interesting coming soon ......")
+streamlit.title("My parents new Healthy Diner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
