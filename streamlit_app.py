b0e14e48ef1a4efa7fe008b01a69f6d88f98886a
sanjyot-mestry <sanjyot.mestry@gmail.com>
2023-11-02 10:16:55 +0000

    Update streamlit_app.py
    added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('FruityVice fruit advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # normalize the json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
