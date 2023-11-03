ff113993793eb995ea447d3fc154fe72d679e8ee
sanjyot-mestry <sanjyot.mestry@gmail.com>
2023-11-03 15:14:16 +0000

    Update streamlit_app.py
    corrected variable name my_data_row to my_data_rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_row)
+  streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add about?')
